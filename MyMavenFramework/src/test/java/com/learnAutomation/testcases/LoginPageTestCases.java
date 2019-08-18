daa79b58d02c2f37443d39b6dfa01c22cb6ce4cf
kuldip.jagtap <kuldip.jagtap@ICPL10171.infostretch.com>
2019-08-18 20:35:52 +0530

    Updated code

--- MyMavenFramework/src/test/java/com/learnAutomation/testcases/LoginPageTestCases.java
+++ MyMavenFramework/src/test/java/com/learnAutomation/testcases/LoginPageTestCases.java
@@ -1,18 +1,29 @@
 package com.learnAutomation.testcases;
 
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import javax.swing.JOptionPane;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import com.learnAutomation.pages.BaseClass;
 import com.learnAutomation.pages.AmazonLoginPage;
 import com.learnAutomation.utility.Listner;
+
+import okio.Timeout;
 
 public class LoginPageTestCases extends BaseClass{
 	
@@ -26,7 +37,7 @@ public class LoginPageTestCases extends BaseClass{
 		//amazonLoginPage.loginToFK(excel.getStringData("Login", 1, 0), excel.getStringData("Login", 1, 1));
 		amazonLoginPage.loginToFK("kjagtap964@gmail.com", "Cool@193");
 		Listner.captureScreenShot(driver);
-		/*logger.pass("Login Success");
+		logger.pass("Login Success");
 		AmazonHomePage amazonHomePage = PageFactory.initElements(driver, AmazonHomePage.class);
 		String handle= driver.getWindowHandle();
 		amazonHomePage.inputSeachItem.sendKeys("Apple iPhone X (256GB) - Space Grey");
@@ -54,7 +65,9 @@ public class LoginPageTestCases extends BaseClass{
 		
 		Actions actions = new Actions(driver);
 		actions.moveToElement(amazonHomePage.labelAccountAndList);
-		actions.moveToElement(amazonHomePage.labelSignOut).click();*/
+		actions.moveToElement(amazonHomePage.labelSignOut).click();
+		
+		
 	}
 	
 
